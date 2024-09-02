d8c34b415b8044722b2ed3e323002dfa755b5cc1
Tarun Mondal <tarunmondal951@gmail.com>
2024-09-02 22:44:51 +0530

    Add files via upload

---
+++ Capstone_1_Project_SQL_File.sql
@@ -0,0 +1,9 @@
+-- Creating database
+create database capstone1;
+use capstone1;
+drop database capstone1;
+select * from stock_raw;
+select * from stock_cleaned;
+select * from model_performance;
+select * from prediction_table;
+
